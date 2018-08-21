c795152fb2897292a62a7800053f362221a35459
kaithiane <kaithiane@hotmail.com>
2018-08-20 22:40:46 -0300

    tp

---
+++ src/Operador/Operadores.java
@@ -0,0 +1,23 @@
+package Operador;
+
+public class Operadores {
+	public static void main(String args[]) {
+		int numero = 10, numero2=1, total=0;
+		total = numero * numero2;
+		
+		if (total > 5) {
+		}else if (total ==5){
+		}
+		
+		int i=0;
+		
+		while(i < 5){
+			
+		for (int x = 5; x > 0; x = x -1) {
+		System.out.println(x);
+		i = i + 1;
+			}
+		}
+	}	
+}		
+				
